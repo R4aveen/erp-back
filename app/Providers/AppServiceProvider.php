a176d3eb957130b99a9784ffe2973768d1c2904e
r4veeen <bas.rojasl@duocuc.cl>
2025-05-11 18:25:40 -0400

    arreglo de appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,17 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Removed invalid parent::boot() call
-
-    Route::aliasMiddleware('permiso', \App\Http\Middleware\CheckPermiso::class);
+        // Ya no se registra aliasMiddleware aquí
     }
-
 }
